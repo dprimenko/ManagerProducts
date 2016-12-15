1d34fc2478cad734871ab4765e6c0ce69880bd19
David Primenko <dprimenko@MacBook-Air-de-David.local>
2016-12-15 20:00:03 +0100

    Changing to fragments

---
+++ app/src/main/java/com/example/managerproducts/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.managerproducts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dprimenko on 15/12/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/managerproducts/fragments/ListProductsFragment.java
@@ -0,0 +1,33 @@
+package com.example.managerproducts.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.managerproducts.R;
+
+/**
+ * Created by dprimenko on 15/12/16.
+ */
+
+public class ListProductsFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_list_products, container, false);
+
+
+
+        return rootView;
+    }
+}
